947df0d110ec4d69b646285a0aa7479fb83e28f3
Cristina <cristina.soltoianu@outlook.com>
2024-02-22 08:10:45 +0100

    Adding  MegaBytes Converter class

--- src/com/solCris/Main.java
+++ src/com/solCris/Main.java
@@ -8,13 +8,26 @@ import static java.lang.System.out;
 public class Main {
 
     public static void main(String[] args) {
-
+        /*
         SpeedConvertor sp = new SpeedConvertor();
-
         double userInput = sp.toMilesPerHour(-1);
         System.out.println("toMilesPerHour " + userInput);
+        sp.printConversion(4); */
 
-        sp.printConversion(4);
+
+        MegaBytesConverter mob= new MegaBytesConverter();
+        mob.printMegaBytesAndKiloBytes(-1);
+        mob.printMegaBytesAndKiloBytes(0);
+        mob.printMegaBytesAndKiloBytes(1024);//should print 1
+        mob.printMegaBytesAndKiloBytes(2048);//should print 2
+        mob.printMegaBytesAndKiloBytes(23552);//should print 23
+        mob.printMegaBytesAndKiloBytes(77777);//should print 75 and 954101562
+
+
+
+
+
+
 
     }
 
