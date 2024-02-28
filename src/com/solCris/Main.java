38b6d83033641c7e3f7e2dcf8e653a8b822b48a3
Cristina <cristina.soltoianu@outlook.com>
2024-02-28 08:17:20 +0100

    Adding ex3: "BarkingDog"
    A simple exercise that returns true if the dog barks after hours or early morning.
    In all other situations returns false.

--- src/com/solCris/Main.java
+++ src/com/solCris/Main.java
@@ -9,12 +9,14 @@ public class Main {
 
     public static void main(String[] args) {
         /*
+        //Uncomment this bit to test Speedconverter
         SpeedConvertor sp = new SpeedConvertor();
         double userInput = sp.toMilesPerHour(-1);
         System.out.println("toMilesPerHour " + userInput);
         sp.printConversion(4); */
 
-
+        /*
+        //Uncomment this bit to test MegaBytesConverter
         MegaBytesConverter mob= new MegaBytesConverter();
         mob.printMegaBytesAndKiloBytes(-1);
         mob.printMegaBytesAndKiloBytes(0);
@@ -23,8 +25,27 @@ public class Main {
         mob.printMegaBytesAndKiloBytes(23552);//should print 23
         mob.printMegaBytesAndKiloBytes(77777);//should print 75 and 954101562
 
+         */
 
+        /*
 
+         */
+        BarkingDog wakeUp = new BarkingDog();
+        //OOB values
+        wakeUp.shouldWakeUp(true, -8); //false
+        wakeUp.shouldWakeUp(true, 65); //false
+
+        //day after waking up hours
+        wakeUp.shouldWakeUp(true, 10); //false
+        wakeUp.shouldWakeUp(true, 9); //false
+        wakeUp.shouldWakeUp(true, 21); //false
+
+        //night after bed
+        out.println("Should all return true");
+        out.println(wakeUp.shouldWakeUp(true, 0)); //true
+        out.println(wakeUp.shouldWakeUp(true, 1)); //true
+        out.println(wakeUp.shouldWakeUp(true, 5)); //true
+        out.println(wakeUp.shouldWakeUp(true, 7)); //true
 
 
 
